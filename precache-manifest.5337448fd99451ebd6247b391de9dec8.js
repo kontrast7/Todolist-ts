eb1a277100bebac7de42881faaa1ae862abfa295
kontrast7 <maks.mashko@gmail.com>
2022-02-19 21:54:38 +0300

    Updates

---
+++ precache-manifest.5337448fd99451ebd6247b391de9dec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a13f54ad582aaaa30bdc6d4ba5199fc",
+    "url": "/Todolist-ts/index.html"
+  },
+  {
+    "revision": "6ffa89eff879afc650b8",
+    "url": "/Todolist-ts/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "2cfcf830a645369fa7b7",
+    "url": "/Todolist-ts/static/js/2.57edb50c.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist-ts/static/js/2.57edb50c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ffa89eff879afc650b8",
+    "url": "/Todolist-ts/static/js/main.616c72d6.chunk.js"
+  },
+  {
+    "revision": "d2ad99ba489a0b93ffcf",
+    "url": "/Todolist-ts/static/js/runtime-main.bcd98c8b.js"
+  }
+]);
